19f6f97cf1857521cbdfc630cbb4880a98143c5d
kushal <kushaldesai1099@ggmail.com>
2021-12-29 10:28:31 +0530

    blank

---
+++ app/src/main/java/com/example/locatetask/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.locatetask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
